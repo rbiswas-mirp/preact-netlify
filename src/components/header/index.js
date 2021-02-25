3df984ca32abea5964e7672f2f258e4da17580fa
rbiswas-mirp <biswasrahul016@gmail.com>
2021-02-25 12:29:12 +0530

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,9 +4,9 @@ import style from './style';
 
 const Header = () => (
 	<header class={style.header}>
-		<Link href="/"><h1>Jane Doe</h1></Link>
+		<Link href="/"><h1>KIIT Confessions</h1></Link>
 		<nav>
-			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
+			<Link activeClassName={style.active} href="/blogs">Confessions</Link>
 			<Link activeClassName={style.active} href="/contact">Contact me</Link>
 		</nav>
 	</header>
